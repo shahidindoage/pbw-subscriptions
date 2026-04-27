7c0cd119a942f395ff457d1495fc268844c9976d
shahidindoage <shahid.indoage@gmail.com>
2026-04-27 12:27:21 +0530

    Changes

--- cron/cron.js
+++ cron/cron.js
@@ -1,5 +1,5 @@
 import cron from "node-cron";
-import { runSubscriptionScheduler } from "./subscriptionScheduler";
+import { runSubscriptionScheduler } from "./subscriptionScheduler.js";
 
 cron.schedule("35,40,45,50,55,60 12 * * *", async () => {
   console.log("⏰ Running (9:05–9:30)");
